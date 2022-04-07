9412da953a30b2b7cd3707ace73590d00c8e353b
Dannspress <danielwalterwork@gmail.com>
2022-04-07 08:28:23 -0300

    VueJS Intro

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+  el: '#main',
+  data: {
+    title: 'THE HACKATHON',
+    names: [
+      'Daniel Walter',
+      'Gabriel Lima',
+      'Gustavo Carvalho',
+      'Vinicius Morselli'
+    ]
+  }
+})
